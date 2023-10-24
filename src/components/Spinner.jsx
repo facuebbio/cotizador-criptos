44f5385b0e217f3b5137754227c0862946796a81
Facundo Ebbio <facuebbio@gmail.com>
2023-10-24 08:53:51 +0200

    First Commit

---
+++ src/components/Spinner.jsx
@@ -0,0 +1,11 @@
+import '../styles/Spinner.css'
+
+export const Spinner = () => {
+  return (
+        <div className="spinner">
+            <div className="bounce1"></div>
+            <div className="bounce2"></div>
+            <div className="bounce3"></div>
+        </div>
+  )
+}
